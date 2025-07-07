7c82e51e4a2e9361b216095e26019ce8b9378bde
Harsh <baps@Mac.lan>
2025-07-08 03:11:13 +0530

    removed s3 cp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,3 @@
 # Install Node.js
 curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
 yum install -y nodejs
-
-# Download the JS file from S3
-aws s3 cp s3://codebuild-practical/main-binary/hello-world.js /tmp/
